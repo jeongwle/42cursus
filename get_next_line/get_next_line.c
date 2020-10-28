55e19254f9a0911d39ecc1e0baf9cda179d47e98
JeongWook Lee <jeongwle@JeongWookui-MacBookPro.local>
2020-10-28 18:04:26 +0900

    caffeconnected

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,30 +6,44 @@
 /*   By: jeongwle <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/24 01:28:12 by jeongwle          #+#    #+#             */
-/*   Updated: 2020/10/26 20:43:10 by jeongwle         ###   ########.fr       */
+/*   Updated: 2020/10/28 18:04:08 by jeongwle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int	get_next_line(int fd, char **line)
+static int	put_line(char **reposit, char **line, char *new_line)
+{
+	char		*temp;
+
+	if (new_line)
+	{
+		*reposit	
+	}
+}
+
+int			get_next_line(int fd, char **line)
 {
 	static char	*reposit;
 	char		*reposit_temp;
+	char		*new_line;
 	char		buf[BUFFER_SIZE + 1];
 	ssize_t		read_val;
 
 	if (!reposit)
 		reposit = ft_strdup("");
-	if (fd < 0 || !line || BUFFER_SIZE < 1)
+	if (fd < 0 || !line || BUFFER_SIZE < 1 ||
+			!(buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1))))
 		return (-1);
-	while (!(ft_strchr(reposit, '\n')) && (read_val = read(fd, buf, BUFFER_SIZE)))
+	while (!(new_line = ft_strchr(reposit, '\n')) && (read_val = read(fd, buf, BUFFER_SIZE)))
 	{
 		buf[read_val] = 0;
 		reposit_temp = reposit;
 		reposit = ft_strjoin(reposit, buf);
 		free(reposit_temp);
 	}
+	if (buf)
+		free(buf);
 	if (read_val < 0)
 		return (-1);
 	reposit_temp = reposit;
